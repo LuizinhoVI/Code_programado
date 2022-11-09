8b8c46cde8a6147ab4a57b5ae4528a598432ebea
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2022-11-09 17:25:27 -0300

    atualização

---
+++ js/action.js
@@ -0,0 +1,8 @@
+function abrir() {
+
+    document.getElementById("perfil1").style.display = "block"
+    document.getElementById("home1").style.display = "none";
+
+    // $('#teste').css({ color: "red", backgroundColor: "#000" });
+
+}
