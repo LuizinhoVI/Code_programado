0c0ce1ee4747b4986015d47b645f0b4c0185e29e
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2022-11-11 10:54:36 -0300

    att

--- js/action.js
+++ js/action.js
@@ -1,7 +1,7 @@
 function abrir() {
 
-    document.getElementById("perfil1").style.display = "block"
-    document.getElementById("home1").style.display = "none";
+    // document.getElementById("perfil1").style.display = "block";
+    // document.getElementById("home1").style.display = "none";
 
     // $('#teste').css({ color: "red", backgroundColor: "#000" });
 
