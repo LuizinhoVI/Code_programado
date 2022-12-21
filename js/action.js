2f300e964d08d1cc77c53dcd343cb415f61aa30a
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2022-12-21 08:07:51 -0300

    tes

--- js/action.js
+++ js/action.js
@@ -1,3 +1,15 @@
+function resetPageOnVisibilityChange() {
+    // Verifica se o status de visibilidade da página mudou para "invisível"
+    if (document.hidden) {
+        // Faz a página recarregar
+        location.reload();
+    }
+}
+
+// Adiciona o evento visibilitychange à página
+document.addEventListener('visibilitychange', resetPageOnVisibilityChange);
+
+
 function abrir() {
 
     // document.getElementById("perfil1").style.display = "block";
